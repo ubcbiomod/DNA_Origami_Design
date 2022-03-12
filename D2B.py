5d2a96d033f3ffc23b915f44665aa69e8bc58da3
Fumiy-2380 <61441690+Fumiy-2380@users.noreply.github.com>
2022-03-12 12:51:40 -0800

    Renamed variable names for better readability
    Changed the names of variables to make the code easier to read.

--- D2B.py
+++ D2B.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 # Parameters
-D = 100                                 # diameter in nm
-nE = 3                                  # number of edges
-nH = 6                                  # number of helices per edge
+diameter = 100                                                    # diameter in nm
+numEdges = 3                                                      # number of edges
+numHelix = 6                                                      # number of helices per edge
 
 
-conv = 0.664                            # conversion factor in nm/nt
-nL = round(D/2*np.sqrt(2).conv)         # length of longer edges in nt
-nS = round(D*np.sin(np.pi/nE)/conv)     # length of shorter edges in nt
-nB = nH*nE*(nL*2+nS)                    # number of nucleotides for the scaffold
-print('nB = ' + str(nB))
+conversion_factor = 0.664                                               # conversion factor in nanometer per nucleotide 
+longSide = round(diameter/2*np.sqrt(2), 3)                              # length of longer edges in nt, round to 3 decimal places
+shortSide = round(diameter*np.sin(np.pi/numEdges)/conversion_factor)    # length of shorter edges in nt
+numBases = numHelix*numEdges*(longSide*2+shortSide)                     # number of nucleotides for the scaffold
+print('Number of Bases = {}'.format(numBases))
 
 #Credits to Qiyang Geng and Ethan Rajkumar
