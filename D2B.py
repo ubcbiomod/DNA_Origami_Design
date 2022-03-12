5d17849b40f5ad7d0cddb8471d1216a8d7988b42
Ethan Rajkumar <61441923+erdabravest2001@users.noreply.github.com>
2022-03-12 11:22:12 -0800

    Update D2B.py

--- D2B.py
+++ D2B.py
@@ -11,3 +11,5 @@ nL = round(D/2*np.sqrt(2).conv)         # length of longer edges in nt
 nS = round(D*np.sin(np.pi/nE)/conv)     # length of shorter edges in nt
 nB = nH*nE*(nL*2+nS)                    # number of nucleotides for the scaffold
 print('nB = ' + str(nB))
+
+#Credits to Qiyang Geng and Ethan Rajkumar
