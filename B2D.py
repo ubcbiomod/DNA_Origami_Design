484261115380a3bc1b05e587f7fd93553180ad48
Fumiy-2380 <61441690+Fumiy-2380@users.noreply.github.com>
2022-03-13 20:13:45 -0700

    Alternate method of N-gonal diameter analysis
    User can now input the number of sides that are on the equatorial axis of the N-gonal bipyramidal spherical liposome scaffold. 
    User can now input the number of helices per 'edge'.

--- B2D.py
+++ B2D.py
@@ -1,15 +1,18 @@
 import numpy as np
 
-arrScaffold = np.array([7249, 7560, 8064])                                                    # number of nucleotides for the scaffold
-numEdges = 3                                                                                  # number of edges
-numHelix = 6                                                                                  # number of helices per edge
+scaffolds = {'M13mp18': 7249, 'p7560': 7560, 'p8064': 8064}                                   # Dictionary of scaffold strands and their lengths
+
+numEdges = int(input("Input the number of edges: "))                                          # number of edges to define n-gonal bipyramidal shape
+numHelix = int(input("Input the number of DNA helices per edge: "))                           # number of helices per edge
 
 
 conversion_factor = 0.332                                                                     # conversion factor length in nm per nucleotide
 cfLong = 1/2*np.sqrt(2)                                                                       # conversion factor for long side (edges on axial (y-axis))
-cfShort = np.sin(np.pi/numEdges)                                                              # conversion factor for short side (edges on equatorial (x-axis)) 
-Diameter_nm = arrScaffold/(numEdges*numHelix*(cfShort+2*cfLong)/conversion_factor)            # diameter in nm
-print('Diameter = {}'.format(Diameter_nm))
+cfShort = np.sin(np.pi/numEdges)                                                              # conversion factor for short side (edges on equatorial (x-axis))
+
+for scaffold in scaffolds.keys():
+    diameter_nm = scaffolds[scaffold]/(numEdges*numHelix*(cfShort+2*cfLong)/conversion_factor)
+    print('Diameter of the spherical {N}-gonal bipyramidal liposome scaffold is {diameter} nm for the {scaf} scaffold {base_len} bases long.'.format(N = numEdges,diameter = diameter_nm, scaf = scaffold, base_len = scaffolds[scaffold]))
 
 
-# Credits to Qiyang Geng and Ethan Rajkumar
+# Credits to Qiyang Geng and Ethan Rajkumar and Fumiya Inaba
